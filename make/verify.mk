0ecc19ae52de777e286b93e80c532389142b0162
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-04-16 15:41:11 +0200

    .github/workflows/verify: remove duplicated mod tidy check
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- make/verify.mk
+++ make/verify.mk
@@ -18,9 +18,3 @@ verify-tidy: fmt ## Verify tidy update.
 .PHONY: verify-generate
 verify-generate: generate ## Verify generate update.
 	git diff --exit-code ./api ./internal
-
-.PHONY: verify-go-mod
-verify-go-mod: ## Verify go.mod matches source code
-	go mod tidy
-	git diff --exit-code ./go.mod
-
